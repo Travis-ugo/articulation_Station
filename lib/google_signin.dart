72dd9158400dbd284174c35c16b59a98b83cfc18
Travis Okonicha <thrinitee@gmail.com>
2021-10-22 21:33:46 +0100

    Firebase Auth

--- lib/google_signin.dart
+++ lib/google_signin.dart
@@ -6,32 +6,57 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 
 class GoogleSign extends StatelessWidget {
-  const GoogleSign({ Key? key }) : super(key: key);
+  const GoogleSign({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-  
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-          const Text('Testing Google signIn'),
-           const Text('Try and erro block'),
-           ElevatedButton.icon(
-           onPressed: () {
-             final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
-             provider.googleLogin();
-           },  
-           icon: const FaIcon(FontAwesomeIcons.google, 
-           color: Colors.red,), 
-           label: const Text('Google sign in'), 
-           style: ElevatedButton.styleFrom(
-             minimumSize: const Size(300, 50),
-             primary: Colors.grey[800],
-           ),),
-        ],),
+            const Text('Testing Google signIn'),
+            const Text('Try and erro block'),
+            ElevatedButton.icon(
+              onPressed: () {
+                final provider =
+                    Provider.of<GoogleSignInProvider>(context, listen: false);
+                provider.googleLogin();
+              },
+              icon: const FaIcon(
+                FontAwesomeIcons.google,
+                color: Colors.red,
+              ),
+              label: const Text('Google sign in'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(300, 50),
+                primary: Colors.grey[800],
+              ),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                // final provider =
+                //     Provider.of<GoogleSignInProvider>(context, listen: false);
+                // provider.signInWithApple();
+              },
+              icon: const FaIcon(
+                FontAwesomeIcons.apple,
+                color: Colors.black54,
+              ),
+              label: const Text(
+                'Apple sign in',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(300, 50),
+                primary: Colors.grey[800],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -39,94 +64,181 @@ class GoogleSign extends StatelessWidget {
 
 class GoogleSignInProvider extends ChangeNotifier {
   // Trigger the authentication flow
-  final googleSignIn =  GoogleSignIn();
+  final googleSignIn = GoogleSignIn();
   GoogleSignInAccount? _user;
-  GoogleSignInAccount get user => _user!; 
+  GoogleSignInAccount get user => _user!;
 
-  Future googleLogin() async{
+  Future googleLogin() async {
     try {
       final googleUser = await googleSignIn.signIn();
-    if (googleUser == null) return;
-    _user = googleUser;
+      if (googleUser == null) return;
+      _user = googleUser;
 
-    final GoogleSignInAuthentication? googleAuth = await googleUser.authentication;
-    final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth?.accessToken,
-    idToken: googleAuth?.idToken,
-     );
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth!.accessToken,
+        idToken: googleAuth.idToken,
+      );
       await FirebaseAuth.instance.signInWithCredential(credential);
-    
     } on FirebaseException catch (e) {
       print(e.toString());
     }
     notifyListeners();
   }
 
-  Future logOut() async{
-    await googleSignIn.disconnect();
-    FirebaseAuth.instance.signOut();
-  }  
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e.toString());
+    }
   }
 
+  // Register user Email and Password
+  Future<void> registerAccount({
+    required String email,
+    required String displayName,
+    required String password,
+  }) async {
+    try {
+      var credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      await credential.user!.updateDisplayName(displayName);
+    } on FirebaseAuthException catch (e) {
+      print(e.toString());
+    }
+  }
+  // /// Generates a cryptographically secure random nonce, to be included in a
+  // /// credential request.
+  // String generateNonce([int length = 32]) {
+  //   const charset =
+  //       '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+  //   final random = Random.secure();
+  //   return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+  //       .join();
+  // }
+
+  // /// Returns the sha256 hash of [input] in hex notation.
+  // String sha256ofString(String input) {
+  //   final bytes = utf8.encode(input);
+  //   final digest = sha256.convert(bytes);
+  //   return digest.toString();
+  // }
+
+  // Future signInWithApple() async {
+  //   // To prevent replay attacks with the credential returned from Apple, we
+  //   // include a nonce in the credential request. When signing in with
+  //   // Firebase, the nonce in the id token returned by Apple, is expected to
+  //   // match the sha256 hash of `rawNonce`.
+  //   try {
+  //     final rawNonce = generateNonce();
+  //     final nonce = sha256ofString(rawNonce);
+
+  //     // Request credential for the currently signed in Apple account.
+  //     final appleCredential = await SignInWithApple.getAppleIDCredential(
+  //       scopes: [
+  //         AppleIDAuthorizationScopes.email,
+  //         AppleIDAuthorizationScopes.fullName,
+  //       ],
+  //       nonce: nonce,
+  //     );
+
+  //     // Create an `OAuthCredential` from the credential returned by Apple.
+  //     final oauthCredential = OAuthProvider("apple.com").credential(
+  //       idToken: appleCredential.identityToken,
+  //       rawNonce: rawNonce,
+  //     );
+
+  //     // Sign in the user with Firebase. If the nonce we generated earlier does
+  //     // not match the nonce in `appleCredential.identityToken`, sign in will fail.
+  //     await FirebaseAuth.instance.signInWithCredential(oauthCredential);
+  //   } on FirebaseException catch (e) {
+  //     print(e.toString());
+  //   }
+  //   notifyListeners();
+  // }
+
+  Future logOut() async {
+    await googleSignIn.disconnect();
+    FirebaseAuth.instance.signOut();
+  }
+}
 
 class Homie extends StatelessWidget {
-  const Homie({ Key? key }) : super(key: key);
+  const Homie({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder( 
-      stream:  FirebaseAuth.instance.authStateChanges(),
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
-        if(snapshot.connectionState == ConnectionState.waiting)
-        {
-          return const  Center(child: CircularProgressIndicator(),);
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text('Something Went wrong'),
+          );
+        } else if (snapshot.hasData) {
+          return const Center(
+            child: LoggedIn(),
+          );
+        } else {
+          return SignInOblack(
+            login: (String email, String password) {},
+            callback: (String email) {},
+          );
         }
-        else  if (snapshot.hasError)
-        {
-          return const  Center(child: Text('Something Went wrong'),);
-        }
-       else  if (snapshot.hasData)
-        {
-          return const  Center(child: LoggedIn(),);
-        }
-        else{
-          return const GoogleSign();
-        }
-       
-      }
+      },
     );
   }
 }
 
-
 class LoggedIn extends StatelessWidget {
-  const LoggedIn({ Key? key }) : super(key: key);
+  const LoggedIn({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser!;
-    return Scaffold(appBar: AppBar(title:const  Text('Logged In'),actions: [
-      TextButton(onPressed: (){
-        final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
-        provider.logOut();
-        }, child: const Text('log Out', style: TextStyle(color: Colors.white),),
-       ),
-      ], 
-    ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-        CircleAvatar(
-        backgroundImage: NetworkImage(user.photoURL!),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Logged In'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              final provider =
+                  Provider.of<GoogleSignInProvider>(context, listen: false);
+              provider.logOut();
+            },
+            child: const Text(
+              'log Out',
+              style: TextStyle(color: Colors.white),
             ),
-        const SizedBox(), 
-        Text(user.phoneNumber.toString()),
-        Text(user.displayName!),
-        Text(user.email!),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(user.photoURL ?? ''),
+            ),
+            const SizedBox(),
+            Text(user.phoneNumber.toString()),
+            Text(user.displayName!),
+            Text(user.email!),
           ],
         ),
-    ),
+      ),
     );
   }
 }
