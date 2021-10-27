891865860884ac6edd9992d9e0b80c7a6ae2e3d6
Travis Okonicha <thrinitee@gmail.com>
2021-10-27 17:44:48 +0100

    Firestore Temper

--- lib/Pages/oblack/home_group_section.dart
+++ lib/Pages/oblack/home_group_section.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:oblack_tech/Pages/oblack/settings.dart';
 import 'package:oblack_tech/Pages/raw/oblack_widgets.dart';
 import 'package:oblack_tech/Widgets/sound_button.dart';
 
@@ -8,73 +7,104 @@ class HomeGroup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> users =
+        FirebaseFirestore.instance.collection('guestbook').snapshots();
     return Scaffold(
       backgroundColor: kolor.backGroundColors,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 35, horizontal: 60),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const SizedBox(width: 0),
-                  const CircleRow(),
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.shopping_cart,
-                        size: 34,
-                        color: Colors.white,
+      body: StreamBuilder<QuerySnapshot>(
+          stream: users,
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Error don sup');
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text('Loading');
+            }
+            final data = snapshot.requireData;
+            return SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 35, horizontal: 60),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const SizedBox(width: 0),
+                        const CircleRow(),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.shopping_cart,
+                              size: 34,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(width: 15),
+                            LButton(onTap: () {}, text: 'Store'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height - 120,
+                      width: MediaQuery.of(context).size.width,
+                      child: GridView.count(
+                        crossAxisCount: 6,
+                        crossAxisSpacing: 30.0,
+                        mainAxisSpacing: 0,
+                        children: List.generate(
+                          data.size,
+                          (index) {
+                            return InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        SelectedSound(sound: data.docs[index]),
+                                  ),
+                                );
+                              },
+                              child: SoundButton(
+                                text: data.docs[index]['Name'],
+                                width: 80,
+                              ),
+                            );
+                          },
+                        ),
                       ),
-                      const SizedBox(width: 15),
-                      LButton(onTap: () {}, text: 'Store'),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height - 120,
-                width: MediaQuery.of(context).size.width,
-                child: GridView.count(
-                  crossAxisCount: 6,
-                  crossAxisSpacing: 30.0,
-                  mainAxisSpacing: 0,
-                  children: List.generate(
-                    19,
-                    (index) {
-                      return const SoundButton(width: 80);
-                    },
-                  ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        LButton(text: 'Info', onTap: () {}),
+                        const SizedBox(width: 15),
+                        LButton(text: 'Score', onTap: () {}),
+                        const SizedBox(width: 15),
+                        LButton(text: 'Group', onTap: () {}),
+                        const SizedBox(width: 15),
+                        LButton(
+                          text: 'Settings',
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const SettingsPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        const SizedBox(width: 15),
+                        LButton(text: 'Multiple', onTap: () {}),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  LButton(text: 'Info', onTap: () {}),
-                  const SizedBox(width: 15),
-                  LButton(text: 'Score', onTap: () {}),
-                  const SizedBox(width: 15),
-                  LButton(text: 'Group', onTap: () {}),
-                  const SizedBox(width: 15),
-                  LButton(
-                      text: 'Settings',
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Settings()));
-                      }),
-                  const SizedBox(width: 15),
-                  LButton(text: 'Multiple', onTap: () {}),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+            );
+          }),
     );
   }
 }
